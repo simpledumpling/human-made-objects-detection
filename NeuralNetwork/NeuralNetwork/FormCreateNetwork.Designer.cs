bcbb01ad0e6ba1c3143b68ee87d844f45daae07a
nast1415 <nast1415@gmail.com>
2015-05-25 00:49:13 +0300

    Project_2015

---
+++ NeuralNetwork/NeuralNetwork/FormCreateNetwork.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace NeuralNetwork
+{
+    partial class FormCreateNetwork
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCreateNetwork));
+            this.gbCreate = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.gbCreate.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbCreate
+            // 
+            this.gbCreate.Controls.Add(this.label2);
+            this.gbCreate.Controls.Add(this.btnCreate);
+            this.gbCreate.Controls.Add(this.numericUpDown3);
+            this.gbCreate.Controls.Add(this.numericUpDown2);
+            this.gbCreate.Controls.Add(this.numericUpDown1);
+            this.gbCreate.Controls.Add(this.lblOutput);
+            this.gbCreate.Controls.Add(this.lblInput);
+            this.gbCreate.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbCreate.Location = new System.Drawing.Point(21, 22);
+            this.gbCreate.Name = "gbCreate";
+            this.gbCreate.Size = new System.Drawing.Size(611, 496);
+            this.gbCreate.TabIndex = 0;
+            this.gbCreate.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Number of hidden layers";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(405, 66);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(185, 52);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(111, 122);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(132, 25);
+            this.numericUpDown3.TabIndex = 5;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(208, 64);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(132, 25);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(23, 64);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(132, 25);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(205, 35);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(140, 17);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "Number of outputs";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(20, 35);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(131, 17);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Number of inputs";
+            // 
+            // FormCreateNetwork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(657, 530);
+            this.Controls.Add(this.gbCreate);
+            this.Font = new System.Drawing.Font("Rockwell", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(673, 280);
+            this.Name = "FormCreateNetwork";
+            this.Text = "Neural Network creation";
+            this.gbCreate.ResumeLayout(false);
+            this.gbCreate.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbCreate;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/FormNetwork.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NeuralNetwork
+{
+    using NeuralNetworksLibrary;
+   
+    public partial class FormNetwork : Form
+    {
+        public FormNetwork()
+        {
+            InitializeComponent();
+        }
+
+        NeuralNetwork NET;
+        String pathToNetwork = "";
+        bool runTraining = false;
+
+        public void CreateNetwork(int SizeX, int[] Layers)
+        {
+            NET = new NeuralNetwork(SizeX, Layers);
+            pathToNetwork = "";
+            txtMessage.AppendText("Neural Network was created\r\n");
+            txtMessage.AppendText("Number of inputs: " + Convert.ToString(SizeX) + "\r\n");
+            txtMessage.AppendText("Number of outputs: " + Convert.ToString(Layers[Layers.Count() - 1]) + "\r\n");
+            txtMessage.AppendText("Number of hidden layers: " + Convert.ToString(Layers.Count() - 1) + "\r\n");
+            
+            for (int i = 0; i < Layers.Count() - 1; i++)
+            {
+                txtMessage.AppendText("Number of neurons in " + Convert.ToString(i + 1) + " hidden layer: "
+                                                    + Convert.ToString(Layers[i]) + "\r\n");
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            tbPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (NET == null)
+            {
+                txtMessage.AppendText("Network is not create!\r\n");
+                return;
+            }
+            txtMessage.AppendText("Training process launched\r\n");
+
+            String strFileIn, strFileOut, strFile;
+
+            // Clean list in training sample
+            txtLearnFiles.Text = "";
+            FileInfo[] fInfo = new DirectoryInfo(tbPath.Text).GetFiles("*.in.txt");
+            foreach (FileInfo f in fInfo)
+            {
+                // Load list of files
+                strFileIn = f.FullName;
+                strFile = strFileIn.Remove(strFileIn.Length - 7);
+                strFileOut = strFile + ".out.txt";
+
+                if (File.Exists(strFileOut) && File.Exists(strFileIn))
+                {
+                    txtLearnFiles.AppendText(strFile + "\r\n");
+                }
+            }
+            if (txtLearnFiles.Lines.Count() > 0)
+                txtMessage.AppendText("Training sample loaded\r\n");
+            else
+            {
+                txtMessage.AppendText("Training sample not found\r\n");
+                return;
+            }
+
+            int currPos = 0;
+            double kErr = 1E256;
+            double kErrNorm = Convert.ToDouble(txtStop.Text);
+            double kLern = Convert.ToDouble(txtSpeed.Text);
+
+            double[] X = new double[NET.GetX];
+            double[] Y = new double[NET.GetY];
+            String[] currFile;
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            runTraining = true;
+            while (kErr > kErrNorm)
+            {
+                kErr = 0;
+                for (currPos = 0; currPos < txtLearnFiles.Lines.Count() - 1; currPos++)
+                {
+                    // Load training pair
+                    try
+                    {
+                        // Load current input file
+                        currFile = File.ReadAllLines(txtLearnFiles.Lines[currPos] + ".in.txt");
+
+                        for (int i = 0; i < NET.GetX; i++)
+                            X[i] = Convert.ToDouble(currFile[i]);
+
+                        // Load current output file
+                        currFile = File.ReadAllLines(txtLearnFiles.Lines[currPos] + ".out.txt");
+
+                        for (int i = 0; i < NET.GetY; i++)
+                            Y[i] = Convert.ToDouble(currFile[i]);
+                    }
+                    finally
+                    { }
+
+                    // Training current pair
+                    kErr += NET.LernNW(X, Y, kLern);
+
+                    Application.DoEvents();
+
+                    if (!runTraining)
+                        return;
+                }
+                txtMessage.AppendText("Current mistake: " + Convert.ToString(kErr) + "\r\n");
+            }
+            txtMessage.AppendText("Training is over!\r\n");
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            runTraining = false;
+        }
+
+        private void neuralNetworkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCreateNetwork f = new FormCreateNetwork();
+            if (f.ShowDialog() != DialogResult.OK)
+            {
+                CreateNetwork(f.getSizeX, f.getLayers);
+            }
+        }
+
+        private void trainingtestingSampleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCreateSample f = new FormCreateSample();
+            f.ShowDialog();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pathToNetwork = openFileDialog1.FileName;
+            if (File.Exists(pathToNetwork))
+            {
+                try
+                {
+                    NET = new NeuralNetwork(pathToNetwork);
+                }
+                finally
+                { }
+
+                txtMessage.AppendText("Network loaded:\r\n" + pathToNetwork + "\r\n");
+
+                txtMessage.AppendText("Number of inputs: " + Convert.ToString(NET.GetX) + "\r\n");
+                txtMessage.AppendText("Number of outputs: " + Convert.ToString(NET.GetY) + "\r\n");
+                txtMessage.AppendText("Number of hidden layers: " + Convert.ToString(NET.CountLayers - 1) + "\r\n");
+
+                for (int i = 0; i < NET.CountLayers - 1; i++)
+                {
+                    txtMessage.AppendText("Neurons in " + Convert.ToString(i + 1) + " hidden layer: "
+                                                        + Convert.ToString(NET.Layer(i).countY) + "\r\n");
+                }
+            }
+            else
+            {
+                if (pathToNetwork != "")
+                {
+                    txtMessage.AppendText("Error! File does not exist!\r\n" + pathToNetwork + "\r\n");
+                    pathToNetwork = "";
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (NET == null)
+            {
+                txtMessage.AppendText("Error! Network is not create!\r\n");
+            }
+            else
+            {
+                if (pathToNetwork == "")
+                {
+                    saveFileDialog1.ShowDialog();
+                    pathToNetwork = saveFileDialog1.FileName;
+                }
+                try
+                {
+                    NET.SaveNW(pathToNetwork);
+                }
+                finally
+                { }
+                txtMessage.AppendText("Network saved:\r\n" + pathToNetwork + "\r\n");
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (NET == null)
+            {
+                txtMessage.AppendText("Error! Network is not create!\r\n");
+            }
+            else
+            {
+                saveFileDialog1.ShowDialog();
+                pathToNetwork = saveFileDialog1.FileName;
+                try
+                {
+                    NET.SaveNW(pathToNetwork);
+                }
+                finally
+                { }
+                txtMessage.AppendText("Network saved:\r\n" + pathToNetwork + "\r\n");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            runTraining = false;
+            txtMessage.AppendText("Training stopped by user\r\n");
+        }
+
+        private void btnOpenTest_Click(object sender, EventArgs e)
+        {
+            if (NET == null)
+            {
+                txtMessage.AppendText("Network is not create!\r\n");
+                return;
+            }
+            openFileDialog2.ShowDialog();
+
+            String strFile = openFileDialog2.FileName;
+
+            if (!File.Exists(strFile))
+                return;
+
+            double[] X = new double[NET.GetX];
+            double[] Y;
+            String[] currFile;
+
+            txtInp.Text = "";
+            txtOutput.Text = "";
+
+            // Load current input
+            currFile = File.ReadAllLines(strFile);
+            txtInp.Lines = currFile;
+            txtMessage.AppendText("File loaded:\r\n" + Convert.ToString(strFile) + "\r\n");
+
+            for (int i = 0; i < NET.GetX; i++)
+            {
+                X[i] = Convert.ToDouble(currFile[i]);
+            }
+
+            NET.NetOUT(X, out Y);
+
+            for (int i = 0; i < NET.GetY; i++)
+            {
+                txtOutput.AppendText(string.Format("{0:F4}\r\n", Y[i]));
+                //textBox2.AppendText(Convert.ToString(Y[i]) + "\r\n");
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (NET == null)
+            {
+                txtMessage.AppendText("Network is not create!\r\n");
+                return;
+            }
+
+            double[] X = new double[NET.GetX];
+            double[] Y;
+
+            txtOutput.Text = "";
+
+            // Загружаем текущий входной файл
+
+            for (int i = 0; i < NET.GetX; i++)
+            {
+                X[i] = Convert.ToDouble(txtInp.Lines[i]);
+            }
+
+            NET.NetOUT(X, out Y);
+
+            for (int i = 0; i < NET.GetY; i++)
+            {
+                txtOutput.AppendText(string.Format("{0:F4}\r\n", Y[i]));
+                //textBox2.AppendText(Convert.ToString(Y[i]) + "\r\n");
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormHelp f = new FormHelp();
+            f.Show();
+        }
+
+    }
+}
